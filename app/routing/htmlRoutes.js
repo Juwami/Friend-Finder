2fbc900f4d3c6e1f390c316640352ecb4eaea560
Juwami <sean.nguyenf@yahoo.com>
2019-07-15 00:46:49 -0400

    add switch case for /survey and /home.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,5 +8,18 @@ let server = http.createServer(routeOpt)
 function routeOpt(req, res) {
     let path = req.url;
 
-    
+    switch(path) {
+        case "/survey":
+            return fs.readFile(__dirname + "/survey.html", function(err, data) {
+                if (err) throw err;
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            })
+        default:
+            return fs.readFile(__dirname + "/home.html", function(err, data) {
+                if (err) throw err;
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            })
+    }
 }
