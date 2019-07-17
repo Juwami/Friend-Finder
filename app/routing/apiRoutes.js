20883df5043d05316d4bea91d4a80056a2f19d38
Juwami <sean.nguyenf@yahoo.com>
2019-07-17 00:03:40 -0400

    create skeleton code for the get and post for /api/friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,12 @@
 let path = require("path")
 
+exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+        res.json()
+    })
+
+    app.post("/apifriends", function(res, req) {
+
+    })
+}
