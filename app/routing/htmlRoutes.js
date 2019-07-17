1517929593e802a88e4770084258a0c8be70f421
Juwami <sean.nguyenf@yahoo.com>
2019-07-17 19:20:47 -0400

    add comment regard the routes to survey.html and home.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,12 +2,14 @@ let path = require("path");
 
 exports = function (app) {
 
-app.get("/survey", function(req, res){
-    res.send(path.join(__dirname, "../public/survey.html"))
-});
+    // route to send to survey.html
+    app.get("/survey", function (req, res) {
+        res.send(path.join(__dirname, "../public/survey.html"))
+    });
 
-app.get("*", function(req, res) {
-    res.send(path.join(__dirname, "../public/home.html"))
-});
+    // default catch all route for anything but survey to go directly to home.html
+    app.get("*", function (req, res) {
+        res.send(path.join(__dirname, "../public/home.html"))
+    });
 
 };
