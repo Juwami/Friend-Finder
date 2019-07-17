4508739e604bd156fd20aef560d76ab7509a1d2d
Juwami <sean.nguyenf@yahoo.com>
2019-07-17 19:29:13 -0400

    require apiroutes and htmlroutes to server js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,12 @@
-let path = require("path")
+let friends = require("../data/friends")
 
-exports = function(app) {
+exports = function (app) {
 
-    app.get("/api/friends", function(req, res) {
-        res.json()
-    })
+    app.get("/api/friends", function (req, res) {
+        res.json(friends)
+    });
 
-    app.post("/api/friends", function(res, req) {
-
-    })
-}
+    app.post("/api/friends", function (req, res) {
+        
+    });
+};
--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 let express = require("express");
 let path = require("path");
 
-let
+let app = express();
+
+let PORT = process.env.PORT || 8000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
