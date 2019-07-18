6bee4c41ed927fd62471ad936f89947066e46851
Juwami <sean.nguyenf@yahoo.com>
2019-07-18 17:59:30 -0400

    add randomly generated data for users

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,137 +1,137 @@
 let friends = [{
         "name": "Stoya",
         "photo": "",
-        "scores": []
+        "scores": [3, 2, 5, 4, 4, 4, 5, 1, 3, 3]
     },
     {
         "name": "Lana Rhoades",
         "photo": "",
-        "scores": []
+        "scores": [2, 5, 3, 4, 4, 3, 1, 5, 3, 2]
     },
     {
         "name": "Jannice Griffith",
         "photo": "",
-        "scores": []
+        "scores": [1, 5, 3, 5, 2, 5, 5, 4, 3, 4]
     },
     {
         "name": "Kayden Kross",
         "photo": "",
-        "scores": []
+        "scores": [2, 4, 3, 2, 1, 2, 5, 1, 2, 1]
     },
     {
         "name": "Madison Ivy",
         "photo": "",
-        "scores": []
+        "scores": [3, 2, 5, 4, 5, 2, 2, 5, 5, 2]
     },
     {
         "name": "Kendra Sunderland",
         "photo": "",
-        "scores": []
+        "scores": [5, 4, 1, 3, 2, 2, 4, 1, 1, 2]
     },
     {
         "name": "Johnny Sins",
         "photo": "",
-        "scores": []
+        "scores": [2, 5, 5, 2, 5, 2, 1, 1, 5, 5]
     },
     {
         "name": "Lexi Belle",
         "photo": "",
-        "scores": []
+        "scores": [2, 4, 5, 4, 5, 1, 5, 1, 2, 3]
     },
     {
         "name": "Alex Grey",
         "photo": "",
-        "scores": []
+        "scores": [3, 4, 1, 4, 1, 5, 1, 5, 5, 2]
     },
     {
         "name": "Dani Daniels",
         "photo": "",
-        "scores": []
+        "scores": [3, 4, 3, 5, 1, 2, 1, 4, 4, 1]
     },
     {
         "name": "Mia Malkova",
         "photo": "",
-        "scores": []
+        "scores": [3, 5, 2, 1, 3, 1, 2, 5, 4, 3]
     },
     {
         "name": "James Deen",
         "photo": "",
-        "scores": []
+        "scores": [5, 5, 3, 3, 2, 1, 5, 2, 2, 2]
     },
     {
         "name": "Xander Corvus",
         "photo": "",
-        "scores": []
+        "scores": [1, 2, 1, 2, 3, 2, 5, 4, 3, 3]
     },
     {
         "name": "Mick Blue",
         "photo": "",
-        "scores": []
+        "scores": [4, 5, 3, 1, 1, 5, 5, 1, 2, 1]
     },
     {
         "name": "Michael Vegas",
         "photo": "",
-        "scores": []
+        "scores": [2, 1, 5, 1, 4, 1, 2, 1, 5, 3]
     },
     {
         "name": "Manuel Ferrara",
         "photo": "",
-        "scores": []
+        "scores": [5, 5, 5, 2, 4, 5, 2, 4, 2, 3]
     },
     {
         "name": "Keiran lee",
         "photo": "",
-        "scores": []
+        "scores": [1, 5, 2, 5, 5, 4, 5, 4, 4, 3]
     },
     {
         "name": "Lisa Ann",
         "photo": "",
-        "scores": []
+        "scores": [5, 3, 4, 4, 1, 3, 2, 1, 4, 5]
     },
     {
         "name": "Nicole Aniston",
         "photo": "",
-        "scores": []
+        "scores": [2, 5, 5, 2, 5, 4, 3, 4, 3, 3]
     },
     {
         "name": "Julia Ann",
         "photo": "",
-        "scores": []
+        "scores": [1, 4, 4, 2, 1, 1, 3, 1, 3, 2]
     },
     {
         "name": "Asa Akira",
         "photo": "",
-        "scores": []
+        "scores": [2, 5, 1, 1, 1, 2, 3, 2, 5, 3]
     },
     {
         "name": "Riley Reid",
         "photo": "",
-        "scores": []
+        "scores": [4, 5, 5, 5, 3, 5, 3, 2, 5, 3]
     },
     {
         "name": "Abella Danger",
         "photo": "",
-        "scores": []
+        "scores": [1, 1, 4, 5, 4, 1, 4, 2, 3, 3]
     },
     {
         "name": "Megan Rain",
         "photo": "",
-        "scores": []
+        "scores": [3, 1, 2, 4, 3, 5, 3, 1, 3, 4]
     },
     {
         "name": "Dillion Harper",
         "photo": "",
-        "scores": []
+        "scores": [2, 2, 3, 5, 4, 3, 1, 1, 5, 4]
     },
     {
         "name": "Sydney Cole",
         "photo": "",
-        "scores": []
+        "scores": [5, 5, 5, 3, 1, 3, 3, 5, 3, 2]
     },
     {
         "name": "Bonnie Rotten",
         "photo": "",
-        "scores": []
+        "scores": [5, 5, 2, 3, 3, 3, 2, 1, 3, 2]
     },
 ];
 
