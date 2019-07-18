8a3516c865a40a1f6feb206c85319d3749b336c1
Juwami <sean.nguyenf@yahoo.com>
2019-07-18 17:30:01 -0400

    put names of mock data instead of comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,38 +1,138 @@
-let friends = [
-    {
-        "name":"Stoya",
+let friends = [{
+        "name": "Stoya",
         "photo": "",
-        "scores": [
-
-        ]
-    }
+        "scores": []
+    },
+    {
+        "name": "Lana Rhoades",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Jannice Griffith",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Kayden Kross",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Madison Ivy",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Kendra Sunderland",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Johnny Sins",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Lexi Belle",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Alex Grey",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Dani Daniels",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Mia Malkova",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "James Deen",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Xander Corvus",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Mick Blue",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Michael Vegas",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Manuel Ferrara",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Keiran lee",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Lisa Ann",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Nicole Aniston",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Julia Ann",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Asa Akira",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Riley Reid",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Abella Danger",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Megan Rain",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Dillion Harper",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Sydney Cole",
+        "photo": "",
+        "scores": []
+    },
+    {
+        "name": "Bonnie Rotten",
+        "photo": "",
+        "scores": []
+    },
 ];
 
 module.exports = friends;
-
-// Lana Rhoades
-// Janice Griffith
-// Kayden Kross
-// Madison Ivy
-// Kendra Sunderland
-// Johnny Sins
-// Lexi Bell
-// Alex Grey
-// Dani Daniels
-// Mia Malkova
-// James Deen
-// Xandere Corvus
-// Mick Blue
-// Michael Vegas
-// Manuel Ferrara
-// Keiran Lee
-// Lisa Ann
-// Nicole Aniston
-// Julia Ann
-// Asa Akira
-// Riley Reid
-// Abella Danger
-// Megan Rain
-// Dillion Harper
-// Sydney Cole
-// Bonnie Rotten
