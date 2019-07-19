8d7f03976267931b7e7cd563b1de8e5ca29b5c7b
Juwami <sean.nguyenf@yahoo.com>
2019-07-18 23:50:40 -0400

    move logic.js into apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,4 +9,8 @@ module.exports = function (app) {
     app.post("/api/friends", function (req, res) {
         friends.push(req.body);
     });
+
+    document.getElementById("submitBtn").onclick = function() {
+        console.log("clicked")
+    }
 };
