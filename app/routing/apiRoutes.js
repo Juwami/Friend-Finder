1af52db6e24da13d745ebe6265dd9436cdd17128
Juwami <sean.nguyenf@yahoo.com>
2019-07-20 00:05:25 -0400

    attempt to get response working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,10 +7,31 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-
-        friends.push(req.body);
-        console.log(req.body);
-
+        console.log(req.body)
+        let user = req.body
+        let bestMatch = null;
+        let highScore = 0;
+        
+        // Console what was inputed
+        // console.log(req.body);
+        
+        for (i=0;i<friends.length;i++) {
+            console.log(user.scores[i])
+            // console.log(friends[i].scores)
+            let differences = 0
+            
+            for (x=0; x<userProfile.scores.length; x++) {
+                differences += Math.abs(userProfile.scores[i] - friends.scores[i])
+            }
+            
+            if (highScore < differences) {
+                highscore = differences;
+                bestMatch = friends[i]
+            }    
+        }
+        friends.push(user);
+        console.log(bestMatch)
+        res.json(bestMatch)
     });
 
     
