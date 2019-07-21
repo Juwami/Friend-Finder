4eca44ed58e5951964a77e7b1499c8ca8668ed2d
Juwami <sean.nguyenf@yahoo.com>
2019-07-20 22:11:04 -0400

    add basic instructions to readme, will touch up later

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.use("/images", express.static(__dirname + '/app/data/images/'))
+app.use("/images", express.static(__dirname + '/appdata/images/'))
 
 console.log(__dirname)
 
